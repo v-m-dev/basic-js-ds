06f5d841391a2b630dd4dd4a6fad9b504ff44f12
v-m-dev <morozvitalii2403@gmail.com>
2023-12-20 10:03:34 +0200

    init: start basic-js-data-structures task

--- src/stack.js
+++ src/stack.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement the Stack with a given interface via array.
@@ -14,21 +14,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 class Stack {
   push(/* element */) {
-    throw new NotImplementedError('Not implemented');
+    throw new NotImplementedError("Not implemented");
     // remove line with error and write your code here
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
+    throw new NotImplementedError("Not implemented");
     // remove line with error and write your code here
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
+    throw new NotImplementedError("Not implemented");
     // remove line with error and write your code here
   }
 }
 
 module.exports = {
-  Stack
+  Stack,
 };
